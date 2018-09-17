59b752f95d846266c39d7ddd4625b4f57fa1f9fc
JihunKwon <jihun.k@frontier.hokudai.ac.jp>
2018-09-17 16:04:05 -0400

    Contour multiple ROIs and save to excel file

--- output_test.m
+++ output_test.m
@@ -1,6 +1,14 @@
+t2map(t2map>100)=nan;
+flag=1;
+while flag
+    try 
+        numofrois=uint8(input('number of ROI: '));
+        flag=0;
+    catch em
+    end
+end
+
 for i=1:size(t2map,4)
-    dirname_crop = '\Users\jihun\Documents\MATLAB\MyImagingLab_T2map_dicom-master\output\247'; %J edit
-%    dirname_crop = '\Users\jihun\Documents\MATLAB\MyImagingLab_T2map_dicom-master\output\249'; %J edit
     [values(:,:,i),b(:,:,:,i),p{i}]=roi_values(t2map,t2map(:,:,1,i),numofrois);
     clf;set(gcf,'Units','normalized','OuterPosition',[0 0 1 1]);
     subplot(1,2,1);imagesc(t2map(:,:,1,1));colormap(gray);img_setting1;title('ROI');hold on
@@ -8,7 +16,41 @@ for i=1:size(t2map,4)
     subplot(1,2,2);plot(values(:,:,i),'LineWidth',2);
     axis_setting1; title('Dynamic T2')
     saveas(gcf,strcat(dirname_crop,'\results\plot_s',num2str(i),'.tif'))
-    out(:,i) = (values(:,:,i));
-    %xlswrite(strcat(dirname_crop,'\results\ROI_values.xlsx'),values(:,:,i),i,'A1');%J: Original code
-    xlswrite(strcat('\results\ROI_values.xlsx'),out,'A1');%J: This works for cropped figures
+    
+    %J: Original code
+    %xlswrite(strcat(dirname_crop,'\results\ROI_values.xlsx'),values(:,:,i),i,'A1');
+    
+    %J: This works for cropped figures
+    out(:,:,i) = (values(:,:,i));
+    sheetname = strcat("Slice ",int2str(i));
+    xlswrite(strcat('\results\ROI_values.xlsx'),out(:,:,i),sheetname);
 end
+
+%{
+t2map(t2map>100)=nan;
+flag=1;
+while flag
+    try 
+        numofrois=uint8(input('number of ROI: '));
+        flag=0;
+    catch em
+    end
+end
+
+for i=1:size(t2map,4)
+    [values(:,:,i),b(:,:,:,i),p{i}]=roi_values(t2map,t2map(:,:,1,i),numofrois);
+    clf;set(gcf,'Units','normalized','OuterPosition',[0 0 1 1]);
+    subplot(1,2,1);imagesc(t2map(:,:,1,1));colormap(gray);img_setting1;title('ROI');hold on
+    roi_para_drawing(p{i},numofrois)
+    subplot(1,2,2);plot(values(:,:,i),'LineWidth',2);
+    axis_setting1; title('Dynamic T2')
+    saveas(gcf,strcat(dirname_crop,'\results\plot_s',num2str(i),'.tif'))
+    
+    %J: Original code
+    %xlswrite(strcat(dirname_crop,'\results\ROI_values.xlsx'),values(:,:,i),i,'A1');
+    
+    %J: This works for cropped figures
+    out(:,i) = (values(:,:,i));
+    xlswrite(strcat('\results\ROI_values.xlsx'),out,'A1');
+end
+%}
